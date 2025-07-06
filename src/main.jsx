a488ee7af9d89be39a9cfda88e526edc8212c402
Sadie <78822295+zerotrustprivacy@users.noreply.github.com>
2025-07-06 12:04:21 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import './index.css' // We'll create this next
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
